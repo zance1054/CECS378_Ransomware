58d493b9f2c585785e94237b1d6db3e4cd73c5e3
Alec <32371145+zance1054@users.noreply.github.com>
2018-10-10 22:37:24 -0700

    Logic for basic encryption with AES

--- fileEncryption.py
+++ fileEncryption.py
@@ -4,12 +4,14 @@
 
 import cryptography
 import os
-from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
 #globals
 
 key = ChaCha20Poly1305.generate_key()
-print(key)
+message = "secret message"
+backend = default_backend()
 #functions
 
 #In this method, you will generate a 16 Bytes IV, and encrypt the message using the key
@@ -17,22 +19,42 @@ print(key)
 #You return an error if the len(key) < 32 (i.e., the key has to be 32 bytes= 256 bits).
 
 def Myencrypt(msg,key):
+    IV = os.random(16)
+    algorithm = algorithms.ChaCha20(key, IV)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend
+    encryptor = cipher.encryptor()
 
-    if(len(key > 32)):
-        print("Invalid key, length must be 32 bytes (256bits)")
-    return 0
+    byteMessage = str.encode(msg)
+
+    padder = padding.PKCS7(blockSize).padder()
+    padded_data = padder.update(byteMessage) + padder.finalize()
+    ct = encryptor.update(padded_data) + encryptor.finalize()
+
+    return [ct, IV]
 
 #In this method, you'll generate a 32Byte key. You open and read the file as a string.
 #You then call the above method to encrypt your file using the key you generated.
 #You return the cipher C, IV, key and the extension of the file (as a string).
 
-def MyfileEncrypt (filepath):
-    return 0
+def invertMyEncrypt(ct):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    decryptor = cipher.decryptor()
+    ct = decryptor.update(ct) + decryptor.finalize()
+    unpadder = padding.PKCS7(blockSize).unpadder()
+    ct = unpadder.update(ct) + unpadder.finalize()
+    ct = str(ct, 'utf-8')
+    print("Decryption: ", ct)
 
-def invertMyEncrypt():
-    return 0
-
-def invertMyfileEncrypt():
-    return 0
 
 #main
+
+key = os.urandom(32)
+if (len(key) < 32):
+    print('Key must be length 32')
+
+(C, IV) =  MyEncrypt(message, key)
+
+
+invertMyEncrypt(C, IV)
+
+#(C, IV, key, ext)= MyfileEncrypt (filepath):
