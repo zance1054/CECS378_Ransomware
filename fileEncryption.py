70262949d64f0a127db43d85641171a069ecf7b7
alec_fielding <alecfielding95@gmail.com>
2018-10-11 12:16:21 -0700

    Working AES CBC encryption / decryption for input string message, moving on to file encrypt/decrypt next

--- fileEncryption.py
+++ fileEncryption.py
@@ -6,27 +6,25 @@ import cryptography
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
 
 #globals
 
-key = ChaCha20Poly1305.generate_key()
 message = "secret message"
-backend = default_backend()
 #functions
 
 #In this method, you will generate a 16 Bytes IV, and encrypt the message using the key
 # and IV in CBC mode (AES).
 #You return an error if the len(key) < 32 (i.e., the key has to be 32 bytes= 256 bits).
 
-def Myencrypt(msg,key):
-    IV = os.random(16)
-    algorithm = algorithms.ChaCha20(key, IV)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend
+def MyEncrypt(msg,key):
+    IV = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
     encryptor = cipher.encryptor()
 
     byteMessage = str.encode(msg)
 
-    padder = padding.PKCS7(blockSize).padder()
+    padder = padding.PKCS7(32).padder()
     padded_data = padder.update(byteMessage) + padder.finalize()
     ct = encryptor.update(padded_data) + encryptor.finalize()
 
@@ -36,14 +34,14 @@ def Myencrypt(msg,key):
 #You then call the above method to encrypt your file using the key you generated.
 #You return the cipher C, IV, key and the extension of the file (as a string).
 
-def invertMyEncrypt(ct):
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+def invertMyEncrypt(C, IV):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
     decryptor = cipher.decryptor()
-    ct = decryptor.update(ct) + decryptor.finalize()
-    unpadder = padding.PKCS7(blockSize).unpadder()
-    ct = unpadder.update(ct) + unpadder.finalize()
-    ct = str(ct, 'utf-8')
-    print("Decryption: ", ct)
+    C = decryptor.update(C) + decryptor.finalize()
+    unpadder = padding.PKCS7(32).unpadder()
+    C = unpadder.update(C) + unpadder.finalize()
+    C = str(C, 'utf-8')
+    print("Decryption: ", C)
 
 
 #main
@@ -54,7 +52,11 @@ if (len(key) < 32):
 
 (C, IV) =  MyEncrypt(message, key)
 
-
 invertMyEncrypt(C, IV)
+
+print(C, IV)
+
+
+#invertMyEncrypt(C, IV)
 
 #(C, IV, key, ext)= MyfileEncrypt (filepath):
