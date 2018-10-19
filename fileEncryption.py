40b4eeb55ea0df21872cc42416b31f15362038da
alec_fielding <alecfielding95@gmail.com>
2018-10-18 18:24:57 -0700

    Menu and improved modularity

--- fileEncryption.py
+++ fileEncryption.py
@@ -1,62 +1,142 @@
 # File Encryption Lab
 # CECS 378
 # Alexander Fielding
+# Phuc Nguyen
 
-import cryptography
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 
-#globals
+#Globals
+keysize = 32
+ivSize = 16
+cipherText = ''
+plainText = ''
+iv = None
+Encryptkey = None
+ext = ""
 
-message = "secret message"
-#functions
+user = input("          Enter user as specified in file manager"+ '\n')
+MAIN_MENU = ('         Select operation.' + '\n' +
+ '          1. Encypt a message from user input' + '\n' +
+ '          2. Encrypt a file' + '\n' +
+ '          3. Decrypt user input message' + '\n' +
+ '          4. Decrypt File' + '\n' +
+ '          5. Quit' + '\n')
 
-#In this method, you will generate a 16 Bytes IV, and encrypt the message using the key
-# and IV in CBC mode (AES).
-#You return an error if the len(key) < 32 (i.e., the key has to be 32 bytes= 256 bits).
+user_input = None
 
-def MyEncrypt(msg,key):
-    IV = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
-    encryptor = cipher.encryptor()
-
-    byteMessage = str.encode(msg)
-
-    padder = padding.PKCS7(32).padder()
-    padded_data = padder.update(byteMessage) + padder.finalize()
-    ct = encryptor.update(padded_data) + encryptor.finalize()
-
-    return [ct, IV]
-
-#In this method, you'll generate a 32Byte key. You open and read the file as a string.
-#You then call the above method to encrypt your file using the key you generated.
-#You return the cipher C, IV, key and the extension of the file (as a string).
-
-def invertMyEncrypt(C, IV):
-    cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
-    decryptor = cipher.decryptor()
-    C = decryptor.update(C) + decryptor.finalize()
-    unpadder = padding.PKCS7(32).unpadder()
-    C = unpadder.update(C) + unpadder.finalize()
-    C = str(C, 'utf-8')
-    print("Decryption: ", C)
-
-
-#main
-
-key = os.urandom(32)
-if (len(key) < 32):
-    print('Key must be length 32')
-
-(C, IV) =  MyEncrypt(message, key)
-
-invertMyEncrypt(C, IV)
-
-print(C, IV)
+# 1. Encryption Method
+def Myencrypt(message,key):
+        # Encoding the String message to b-8bytes binary
+    messageB = message
+        # Catching exception when the key length < 32 and print out
+    if(len(key) < keysize):
+        print("Invalid key, length must be 32 bytes (256bits)")
+        return
+        # Padding using PKCS37, symmetric padding
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
+    padded_plainText = padder.update(messageB) # update the plain text to padded message
+    padded_plainText += padder.finalize()
+        # Now, move to encrypting the padded_plainText
+    iv = os.urandom(ivSize); # create the iv
+        # encrypting using AES algorithms and CBC modes
+    cipherEncrypt = (Cipher(algorithms.AES(key), modes.CBC(iv), backend = default_backend())).encryptor()
+        #Then update the encrypt method with the padded plain text message and finalize it
+    cipherText = cipherEncrypt.update(padded_plainText) + cipherEncrypt.finalize()
+    return(cipherText, iv)
 
 
-#invertMyEncrypt(C, IV)
 
-#(C, IV, key, ext)= MyfileEncrypt (filepath):
+# 2. Decryption Method - Inverse of Encryption
+def Mydecrypt(cipherText, key,iv):
+    # Catching exception when the key length < 32 and print out
+    if(len(key) < keysize):
+        print("Invalid key, length must be 32 bytes (256bits)")
+    # Decrypt the cipher Text to padded plainText
+    cipherDecrypt = (Cipher(algorithms.AES(key), modes.CBC(iv), backend = default_backend())).decryptor()
+    padded_plainText = cipherDecrypt.update(cipherText) + cipherDecrypt.finalize()
+
+    # Then, unpad the padded plainText into actual message that is the same as before we encrypted
+    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+    message = unpadder.update(padded_plainText)
+    message += unpadder.finalize()
+    # decoding to string message
+    plainText = message
+    return plainText
+
+# 3. Encrypting to a file path ( I set it to .txt so we don't have to open with every time)
+def MyFileEncrypt(filepath):
+    # generate a random key
+    key = os.urandom(keysize)
+    print("32 bytes key: ", key)
+    plainTextFile = open(filepath, 'rb');
+    message = plainTextFile.read()
+    (cipherText, iv) = Myencrypt(message,key)
+    messageEncrypted = open(filepath + "Encrypt.txt", 'wb')
+    messageEncrypted.write(cipherText)
+    return cipherText, iv, key, "Encrypt.txt"
+
+# 4. Inverse of encrypting to file, this method lets us decrypt the cipher text from the encrypted file
+def MyFileDecrypt(filepath, key, iv):
+    messageEncrypted = open(filepath, 'rb')
+    cipherText = messageEncrypted.read()
+    plainText = Mydecrypt(cipherText, key, iv)
+    cipherTextDecrypted = open(filepath + "Decrypt.txt", 'wb')
+    print('Decypted message from file:' + '\n')
+    cipherTextDecrypted.write(plainText)
+
+# Main
+
+# 1. Testing the Myencrypt and Mydecrypt method by encrypt a message then decrypt and compare if the boolean return true for matching plaintext
+while(user_input != 5):
+
+    print(MAIN_MENU)
+
+    user_input = int(input("Enter a number to execute an operation from above" + '\n'))
+
+    if(user_input == 1):
+        Encryptkey = os.urandom(keysize) # creating a key that is 32 bytes
+        plainText = input("           Enter the message you want to encrypt" + '\n')
+
+        bytemsg = str.encode(plainText)
+
+        print("         Plain Text:", plainText)
+
+        (cipherText,iv) = Myencrypt(bytemsg, Encryptkey)
+
+        print("Cipher Text:", cipherText)
+
+        print("IV:", iv)
+
+        pt = Mydecrypt(cipherText,Encryptkey, iv)
+
+        print("Decrypted Message:", pt)
+        print('\n')
+
+    elif(user_input == 2):
+
+        # 2. Test the file encryption and decryption
+        filepath = "/Users/" + user + "/Desktop/read.txt"
+        #encrypting the message to a filepath.encrypt
+        (cipherText, iv, Encryptkey, ext) = MyFileEncrypt(filepath)
+
+        print("Cipher Text:", cipherText)
+
+    elif(user_input == 3):
+
+        plainText = Mydecrypt(cipherText,Encryptkey, iv)
+
+        print("Decrypted Message:", plainText)
+        print('\n')
+
+    elif(user_input == 4):
+        #decrypting
+        MyFileDecrypt(filepath + ext, Encryptkey, iv)
+
+    elif(user_input == 5):
+        break;
+
+    else:
+        print("         Invalid input")
